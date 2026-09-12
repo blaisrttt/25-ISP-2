f23bd80bab1990fd87d5cda5c58d9f41dd07c037
blaisrtt <blaisrtt@511-01>
2026-09-12 13:48:17 +0200

    Добавьте файлы проекта.

---
+++ lab1_1/Program.cs
@@ -0,0 +1,6 @@
+﻿//высокий
+
+Console.Write("Введите х:");
+double x = double.Parse(Console.ReadLine());
+double K = (Math.Sqrt(Math.Pow(3 + x, 6) - Math.Log(x))) / (Math.Exp(0) + Math.Asin(6 * x * x));
+Console.WriteLine($"K={K:F2}");
